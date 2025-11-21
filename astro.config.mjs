7f3f27ebb4a2343f928f00ea40911297468f10ce
edbpede <144238562+edbpede@users.noreply.github.com>
2025-11-21 16:49:49 +0100

    feat: install unocss
    Integrate UnoCSS for styling Astro project.
    
    This commit sets up UnoCSS as an Astro integration and creates a default UnoCSS configuration file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import solidJs from '@astrojs/solid-js';
+import UnoCSS from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  integrations: [
+    UnoCSS(),
+    solidJs()
+  ]
 });
